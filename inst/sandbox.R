2de0a4bdb747e7c8cb97e2bdd5dd9fbf8a8e143c
mbannert <matthias.bannert@gmail.com>
2017-12-03 08:13:24 +0100

    initial function to get indices that we can plot with dygraphs

---
+++ inst/sandbox.R
@@ -0,0 +1,43 @@
+library(quantmod)
+library(dygraphs)
+
+get_stock_matrix <- function(ti_symbols = c("AAPL",
+                                         "MSFT",
+                                         "GOOG"),
+                             src = "yahoo",
+                             start = "2015-01-01",
+                             end = Sys.Date(),
+                             metric = ".Close"){
+  stock_env <- new.env()
+  start <- as.Date(start)
+  end <- as.Date(end)
+  
+  
+  
+  getSymbols(ti_symbols, src = src,
+             from = start, to = end,
+             env = stock_env)
+  
+  stock_l <- as.list(stock_env)
+  stock_l <- lapply(ti_symbols, function(x){
+    sel_metric <- paste0(x,metric)
+    stock_l[[x]][,sel_metric]
+  })
+
+  # return a matrix of stocks
+  # which can easily be converted to xts
+  as.matrix(as.xts(do.call("cbind",stock_l)))
+}
+
+create_index <- function(xts_mat,base_period = 1,
+                         times = 100){
+  out <- t(t(xts_mat) / xts_mat[base_period,])
+  if(!is.null(times)) out <- out * times
+  out
+}
+
+smat <- get_stock_matrix()
+rest <- create_index(smat,times = 100)
+dygraph(rest)
+
+
